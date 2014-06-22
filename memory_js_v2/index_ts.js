3e3cb337848492210029bf675ee2da45dc9fca08
tsubakimoto_s <tkmbmbr@gmail.com>
2014-06-22 22:33:06 +0900

    TypeScript版の作成

---
+++ memory_js_v2/index_ts.js
@@ -0,0 +1,43 @@
+﻿var Card = (function () {
+    function Card(num) {
+        this.Initialize(num);
+    }
+    Card.prototype.Initialize = function (num) {
+        var _this = this;
+        var card = document.createElement('input');
+        card.type = 'button';
+        card.value = '?';
+        card.dataset.num = num;
+        card.onclick = function () {
+            _this.Flip();
+        };
+        this.button = card;
+    };
+
+    Card.prototype.Flip = function () {
+        alert('flip!');
+    };
+    return Card;
+})();
+
+var cards = [], CARD_NUN = 16, currentNum, openedCard, correctNum = 0, enableFlip = true, score = 0, timerId;
+
+window.onload = function () {
+    var cardIndex;
+    var stage = document.getElementById('stage');
+
+    for (var i = 0; i < CARD_NUN; i++) {
+        var num = Math.floor(i / 2);
+        do {
+            cardIndex = Math.floor(Math.random() * CARD_NUN);
+        } while(typeof cards[cardIndex] !== 'undefined');
+        cards[cardIndex] = new Card(num).button;
+    }
+    for (var j = 0; j < CARD_NUN; j++) {
+        stage.appendChild(cards[j]);
+        if (j % Math.sqrt(CARD_NUN) == (Math.sqrt(CARD_NUN) - 1)) {
+            stage.appendChild(document.createElement('br'));
+        }
+    }
+};
+//# sourceMappingURL=index_ts.js.map
---
+++ memory_js_v2/index_ts.ts
@@ -0,0 +1,59 @@
+﻿interface ICard {
+    button: HTMLInputElement;
+    value: string;
+
+    Initialize(num: number);
+    Flip();
+}
+
+class Card implements ICard {
+    button: HTMLInputElement;
+    value: string;
+
+    constructor(num: number) {
+        this.Initialize(num);
+    }
+
+    Initialize(num: number) {
+        var card = document.createElement('input');
+        card.type = 'button';
+        card.value = '?';
+        card.dataset.num = num;
+        card.onclick = () => {
+            this.Flip();
+        }
+        this.button = card;
+    }
+
+    Flip() {
+        alert('flip!');
+    }
+}
+
+var cards = [],
+    CARD_NUN = 16,
+    currentNum,
+    openedCard,
+    correctNum = 0,
+    enableFlip = true,
+    score = 0,
+    timerId;
+
+window.onload = () => {
+    var cardIndex;
+    var stage = document.getElementById('stage');
+
+    for (var i = 0; i < CARD_NUN; i++) {
+        var num = Math.floor(i / 2);
+        do {
+            cardIndex = Math.floor(Math.random() * CARD_NUN);
+        } while (typeof cards[cardIndex] !== 'undefined');
+        cards[cardIndex] = new Card(num).button;
+    }
+    for (var j = 0; j < CARD_NUN; j++) {
+        stage.appendChild(cards[j]);
+        if (j % Math.sqrt(CARD_NUN) == (Math.sqrt(CARD_NUN) - 1)) {
+            stage.appendChild(document.createElement('br'));
+        }
+    }
+}
